94ef09309be54d96ddf7d4fc96b160e91873bdf3
ZenGaming2010 <159475194+ZenGaming2010@users.noreply.github.com>
2024-02-08 19:23:02 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Spidey-Hub","Watching Spidey-Hub","Watching Spidey-Hub"];
 
 
 let currentIndex = 0;
